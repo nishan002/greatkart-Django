c4bc4d5b95f5c96243245a0509a200491de6e793
nishan002 <md.nishanahmed14@gmail.com>
2022-02-02 14:15:14 +0600

    first commit

---
+++ greatkart/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from product.models import Product
+from product.models import Product
+
+def home(request):
+    products = Product.objects.all().filter(is_available=True)
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'home.html', context)
